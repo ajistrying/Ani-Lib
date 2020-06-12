78758e10288a714cf6ccae769f523893b4a480c7
Wellington Johnson <ajistrying@gmail.com>
2020-06-11 22:50:49 -0400

    Initial commit to repo TODO: Start creating layout of the collection view of the home view controller

---
+++ AniLib/Controllers/HomeViewController.swift
@@ -0,0 +1,24 @@
+//
+//  HomeViewController.swift
+//  AniLib
+//
+//  Created by Wellington Johnson on 6/10/20.
+//  Copyright © 2020 Wellington Johnson. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+    @IBOutlet var animeCollectionView: UICollectionView!
+    @IBOutlet var mangaCollectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+    
+
+}
---
+++ AniLib/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AniLib
+//
+//  Created by Wellington Johnson on 6/10/20.
+//  Copyright © 2020 Wellington Johnson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
